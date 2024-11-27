8896d36cd4ba8d62e35d1d447feaf333597d8323
Midha Eduction <midhaeduction@Midhas-MacBook-Air.local>
2024-11-27 12:26:28 +0530

    add

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const { connection }= require('./db.js')
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req,res)=>{
-    res.send("Api is running")
+    res.send("Api is running successfully")
 })
 
-const PORT = process.env.PORT ||5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(PORT,async()=>{
+    try {
+        await connection;
+        console.log("DB is connected");
+        console.log(`Server is running on http://localhost:${PORT}`);
+    } catch (error) {
+        console.log(error)
+    }
+   
 })
